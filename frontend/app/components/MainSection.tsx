caecaa061b8e15c07ef9e58b197e4bd62fdec545
sakti8093 <saktiprasad8093@gmail.com>
2023-06-15 19:53:56 +0530

    fix crashing issue

--- frontend/app/components/MainSection.tsx
+++ frontend/app/components/MainSection.tsx
@@ -16,7 +16,7 @@ const MainSection = ({ title, prop }: props) => {
     <div className="p-4">
       <p>{title}</p>
       <div className="w-full flex overflow-x-scroll space-x-4">
-        {prop.map((elem)=>(
+        {prop?.map((elem)=>(
              <div key={elem.id} className="relative w-[200px] h-[200px] flex-shrink-0">
                 <Image  className="object-cover" fill alt="" src={`${base_url}${elem.backdrop_path || elem.poster_path}`} />
              </div>
