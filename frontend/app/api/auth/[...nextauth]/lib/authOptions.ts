600724573e4151b7062ad7fc0a1a9c9dc93258d2
sakti8093 <saktiprasad8093@gmail.com>
2023-06-27 20:27:16 +0530

    updated env files

--- frontend/app/api/auth/[...nextauth]/lib/authOptions.ts
+++ frontend/app/api/auth/[...nextauth]/lib/authOptions.ts
@@ -5,8 +5,8 @@ export const authOptions:NextAuthOptions = {
     providers: [
       // OAuth authentication providers...
       GoogleProvider({
-        clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID??"",
-        clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET??""
+        clientId: process.env.GOOGLE_CLIENT_ID??"",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET??""
       }),
     ]
 } 
